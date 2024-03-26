8b1203ab3f64955e006beb689bca0f036d55791a
Sam Albers <sam.albers@gmail.com>
2024-03-26 09:13:48 -0700

    clean up

--- post-process.R
+++ post-process.R
@@ -1,8 +1,10 @@
+
+## Remove line created in pre-process.R
 con <- file("_environment", open = "r")
-
 env_file <- readLines(con)
-
 no_month <- env_file[!grepl("MONTH=", env_file)]
-
 write(no_month, file = "_environment", sep = "\n")
 close(con)
+
+## Remove file created in pre-process.R
+unlink("_environment-dynamic")
--- pre-process.R
+++ pre-process.R
@@ -1,9 +1,8 @@
-month_dynamic <- paste0("MONTH_DYNAMIC='", format(Sys.Date(), "%b"), "'")
-month <- paste0("MONTH='", format(Sys.Date(), "%b"), "'")
+## Trying _environment-dynamic
 
+month_dynamic <- paste0("MONTH_DYNAMIC='", format(Sys.Date(), "%b"), "'")
 writeLines(month_dynamic, "_environment-dynamic")
 
-
+## Try to add to _environment
+month <- paste0("MONTH='", format(Sys.Date(), "%b"), "'")
 write(month, file = "_environment", append = TRUE, sep = "\n")
-
-unlink("_environment-dynamic")
